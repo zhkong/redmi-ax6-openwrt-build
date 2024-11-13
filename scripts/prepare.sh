6dad62572b7539b369bfd5adc0b61eff7eb1ec06
klarkzh <klarkzh@hotmail.com>
2024-11-13 13:00:40 +0800

    switch to posmio/openwrt-ipq

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,11 +2,12 @@
 # @Author: zhkong
 # @Date: 2023-07-25 17:07:02
  # @LastEditors: zhkong
- # @LastEditTime: 2024-03-22 22:11:33
- # @FilePath: /xiaomi-ax6-openwrt-build/scripts/prepare.sh
+ # @LastEditTime: 2024-11-13 12:59:20
+ # @FilePath: /redmi-ax6-openwrt-build/scripts/prepare.sh
 ###
 
-git clone https://github.com/AgustinLorenzo/openwrt.git --single-branch openwrt --depth 1
+# git clone https://github.com/AgustinLorenzo/openwrt.git --single-branch openwrt --depth 1
+git clone https://github.com/qosmio/openwrt-ipq.git -b qualcommax-6.x-nss-wifi --single-branch openwrt --depth 1
 cd openwrt
 
 # 更新 Feeds
