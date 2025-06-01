348a9ede6763f85779b49e1cfc257092012e9f09
klarkzh <klarkzh@hotmail.com>
2025-06-01 10:07:47 +0800

    fix

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,13 +2,13 @@
 # @Author: zhkong
 # @Date: 2023-07-25 17:07:02
  # @LastEditors: zhkong
- # @LastEditTime: 2025-03-20 04:05:33
+ # @LastEditTime: 2025-05-18 01:53:59
  # @FilePath: /redmi-ax6-openwrt-build/scripts/prepare.sh
 ###
 
-git clone https://github.com/AgustinLorenzo/openwrt.git -b main_nss --single-branch --depth 1 openwrt
+# git clone https://github.com/AgustinLorenzo/openwrt.git -b main_nss --single-branch --depth 1 openwrt
 # git clone https://github.com/qosmio/openwrt-ipq.git -b 24.10-nss --single-branch --depth 1 openwrt
-# git clone https://github.com/openwrt/openwrt.git -b v24.10.0 --single-branch --depth 1 openwrt
+git clone https://github.com/openwrt/openwrt.git -b v24.10.0 --single-branch --depth 1 openwrt
 cd openwrt
 
 # 更新 Feeds
@@ -61,7 +61,8 @@ mv temp/immortalwrt/package/emortal/default-settings package/new/default-setting
 # bash ../scripts/preset-terminal-tools.sh
 
 # config file
-cp ../config/redmi-ax6/AgustinLorenzo-openwrt .config
+# cp ../config/redmi-ax6/AgustinLorenzo-openwrt .config
+cp ../config/redmi-ax6/openwrt-official .config
 make defconfig
 
 rm -rf temp
